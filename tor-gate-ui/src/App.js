da31fa8b1cf8ca0a29b7f55161e4cbc7d4edd2d9
Sergei Malykh <SVMalykh@sberbank.ru>
2021-10-07 21:24:17 +0300

    ui: initial commit

---
+++ tor-gate-ui/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Test app
+    </div>
+  );
+}
+
+export default App;
---
+++ tor-gate-ui/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import View from "./View";
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <View />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
